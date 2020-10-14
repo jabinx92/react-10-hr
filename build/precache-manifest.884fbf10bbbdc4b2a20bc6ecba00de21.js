ed53e86de64ff0057bb6b7c9c837d3b4869355df
Jonathan Won <jabinx92@gmail.com>
2020-10-13 23:50:49 -0700

    first commit

---
+++ build/precache-manifest.884fbf10bbbdc4b2a20bc6ecba00de21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26436f55a101a4459fe8f5962ce61308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63b0dea3dd161b8ebc4f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c21ee649eb5c49f1961",
+    "url": "/static/js/2.41f5d406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41f5d406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b0dea3dd161b8ebc4f",
+    "url": "/static/js/main.2749640d.chunk.js"
+  },
+  {
+    "revision": "1877e69cc92e77fffa19",
+    "url": "/static/js/runtime-main.147a4b9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
